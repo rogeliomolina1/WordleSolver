9d99fcb5e568016f305c5bfff2a91a4130e2e6df
Rogelio Molina <rogelio.molina1@gmail.com>
2023-06-18 17:31:30 -0700

    moved code to functions

--- WordSolver.py
+++ WordSolver.py
@@ -1,17 +1,27 @@
 import nltk
 
-# Download the required resources from NLTK
-nltk.download('words')
+def createWordList():
+    # Download the required resources from NLTK
+    nltk.download('words')
 
-# Load the word list
-word_list = nltk.corpus.words.words()
-five_letter_words = [word.lower() for word in word_list if len(word) == 5]
+    # Load the word list
+    words = nltk.corpus.words.words()
+    five_letter_words = [word.lower() for word in words if len(word) == 5]
 
-# Sort words by popularity
-word_freq = nltk.FreqDist(five_letter_words)
-sorted_words = sorted(word_freq.items(), key=lambda x: x[1], reverse=True)
+    # Sort words by popularity
+    word_freq = nltk.FreqDist(five_letter_words)
+    sorted_words = sorted(word_freq.items(), key=lambda x: x[1], reverse=True)
 
-# Print a sample of the first 10 words
-print("Sample of the first 10 words:")
-for word, frequency in sorted_words[:10]:
-    print(f"Word: {word}, Frequency: {frequency}")
+    return sorted_words
+    
+def main():
+
+    word_list = createWordList()
+
+    # Print a sample of the first 10 words
+    print("Sample of the first 10 words:")
+    for word, frequency in word_list[:10]:
+        print(f"Word: {word}, Frequency: {frequency}")
+
+if __name__ == "__main__":
+    main()
